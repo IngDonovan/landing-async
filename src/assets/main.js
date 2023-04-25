513285589cf6283b87f4bf489dc87abc0c70cc2d
Donovan Rojas <rojasdrm03@gmail.com>
2023-04-25 16:44:32 -0500

    add channel of cssTricks and  see videos to youtube with rapidapi

---
+++ src/assets/main.js
@@ -0,0 +1,54 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCADyUOnhyEoQqrw_RrsGleA&part=snippet%2Cid&order=date&maxResults=10';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'content-type': 'application/octet-stream',
+		'X-RapidAPI-Key': 'a1305db9e6mshbbfd77999cbfccfp10898ejsnebfabe908c5a',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
+
+async function fetchData(urlApi) {
+	const response = await fetch(urlApi, options);
+	const data = await response.json();
+	return data;
+}
+
+//funcion que se invoca a si misma, funcion anonima
+(async () => {
+	try {
+		const videos = await fetchData(API);
+		//generamos un template
+		let view = `
+		${videos.items.map(video => `
+			<div class="group relative">
+				<div
+					class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+					<img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+				</div>
+				<div class="mt-4 flex justify-between">
+					<h3 class="text-sm text-gray-700">
+						<span aria-hidden="true" class="absolute inset-0"></span>
+						${video.snippet.title}
+					</h3>
+				</div>
+			</div>
+		`).slice(0,4).join('')}
+		
+		`;
+	} catch (error) {
+		
+	}
+})();
